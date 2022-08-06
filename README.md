577ee0fd25a7fd6f7f6d8f48f4fcfee489d04ae0
Enzuo <potc.zone+git@gmail.com>
2022-08-06 16:12:44 +0200

    Set api to generate a random question

--- README.md
+++ README.md
@@ -3,3 +3,10 @@
 
 ## Start
 `npm run dev`
+
+## Create movie clips
+
+```bash
+ffmpeg -i idiocracy.mp4 -ss 00:00:00 -to 00:00:24 -c:v libvpx-vp9 -b:v 0.8M -pass 1 -an -f null NUL && ^
+ffmpeg -i idiocracy.mp4 -ss 00:00:00 -to 00:00:24 -c:v libvpx-vp9 -b:v 0.8M -pass 2 -c:a libopus output.webm
+```
--- server/api.js
+++ server/api.js
@@ -2,8 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 
-const data = [
-    {title: "jibaro", file : "jibaro2_nqjs1w"}
+const dataClips = [
+    {title: "jibaro", file : "jibaro2_nqjs1w"},
+    {title: "mr bean ultimate disaster", file : "ultimatedisaster"},
+    {title: "idiocracy", file : "idiocracyiq"},
 ]
 
 router.get('/', function (req, res) {
@@ -11,8 +13,19 @@ router.get('/', function (req, res) {
 })
 
 router.get('/getQuestion', function (req, res) {
-    let id = 0
-    res.json({file: data[id].file });
+    const question = getAQuestion(dataClips)
+    res.json({id: question.id, file: question.file })
 })
+
+router.post('/answer', function (req, res) {
+
+})
+
+// Logic 
+
+function getAQuestion(data){
+    let id = Math.floor(Math.random()*data.length)
+    return {id: id, file: data[id].file }
+}
 
 module.exports = router;
