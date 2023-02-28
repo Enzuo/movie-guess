3f0dc915981843ccb7781afdae6b97691be237ea
Enzuo <potc.zone+git@gmail.com>
2023-02-28 13:23:29 +0100

    Use animationFrame to animate svg

--- src/components/CountDownTimer.jsx
+++ src/components/CountDownTimer.jsx
@@ -1,8 +1,9 @@
-import { useState, useEffect, useReducer } from "react"
+import { useState, useEffect, useReducer, useRef } from "react"
 
 export default function CountdownTimer({ targetTime }) {
 
   const [countDownLength, setCountDownLength] = useState(10)
+  const pathRef = useRef(null)
 
   // TODO Fix unpure reducer
   const [countDown, updateCountDown] = useReducer((state, action) => {
@@ -12,19 +13,45 @@ export default function CountdownTimer({ targetTime }) {
   },
   targetTime - new Date().getTime())
 
-  useEffect(() => {
-    setCountDownLength(targetTime - new Date().getTime())
-    updateCountDown()
-    const interval = setInterval(() => {
-      updateCountDown();
-      const currentTime = new Date().getTime()
-      if(targetTime - currentTime <= 0){
-        clearInterval(interval)
-      }
-    }, 1000);
+  // useEffect(() => {
+  //   setCountDownLength(targetTime - new Date().getTime())
+  //   updateCountDown()
+  //   const interval = setInterval(() => {
+  //     updateCountDown();
+  //     const currentTime = new Date().getTime()
+  //     if(targetTime - currentTime <= 0){
+  //       clearInterval(interval)
+  //     }
+  //   }, 1000);
 
-    return () => clearInterval(interval);
-  }, [targetTime]);
+  //   return () => clearInterval(interval);
+  // }, [targetTime]);
+
+  useEffect(() => {
+    let animationFrameId
+    const animateFn = () => {
+
+      if(pathRef){
+
+        const currentTime = new Date().getTime()
+        const time = targetTime - currentTime
+        const ct = time > 0 ? time : 0
+        const percent = ct/(countDownLength*1000)
+        const radius = 360*percent
+
+        pathRef.current.setAttribute("d", describeArc(14, 14, 12, 0, radius))
+      }
+
+
+
+      animationFrameId = requestAnimationFrame(animateFn)
+    }
+    
+
+    animationFrameId = requestAnimationFrame(animateFn)
+    return () => cancelAnimationFrame(animationFrameId);
+
+  }, [targetTime])
 
 
   const seconds = Math.floor((countDown % (1000 * 60)) / 1000);
@@ -44,6 +71,7 @@ export default function CountdownTimer({ targetTime }) {
             d={describeArc(14, 14, 12, 0, 359)}
         />
         <path
+            ref={pathRef}
             fill="none"
             stroke={color}
             strokeWidth="4"
